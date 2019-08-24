9e572bd41a255ca7b528eea7d47c848d70f3b397
Syed Azam Hassan <kashif.hassan@gmail.com>
2019-08-24 10:27:54 +0500

    max consecutive repeating chr in string

--- Length_of_Longest_Repeating.py
+++ Length_of_Longest_Repeating.py
@@ -1,9 +1,20 @@
-def l(s):
+def l1(s):
     mm = cnt = 0;cur = ''
     for c in s:
         if c == cur: cnt += 1
         else: cnt = 1;cur = c
         mm = max(cnt,mm)
-    return mm > 2
+    return mm
 
-print(l("abbb"))
+def l2(s):
+    b = s[0]
+    max_cnt = cnt = 0
+    for c in s:
+        cnt = cnt+1 if c == b else 1
+        b = c
+        max_cnt = cnt if max_cnt < cnt else max_cnt
+    return max_cnt
+
+print(l1("aaaabbaabbbccbbde"))
+print(l2("aaaabbaabbbccbbde"))
+input()
